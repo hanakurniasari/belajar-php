0ab69c87eca312dba0588fd977859384fc03415b
Hana Kurniasari <Kurniasarihana@gmail.com>
2022-10-24 17:52:33 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Hana Kurniasari";
+$usia = 22;
+
+echo "Nama saya $nama, saya berusia $usia tahun.<br>";
+
+$namakakak = "Kurnia Ramadani";
+$usiakakak = 25;
+
+echo "Saya memiliki satu orang kakak perempuan yang bernama $namakakak, kakak saya berusia $usiakakak tahun.<br>";
+
+
+$selisihumur = $usiakakak - $usia;
+echo "Selisih usia saya dan kakak saya adalah $selisihumur tahun.<br>";
+
+
+$usiaibu = 50;
+$usiajumlah = $usiakakak + $usia;
+echo "Jika dijumlahkan maka jumlah total umur kami adalah $usiajumlah tahun.<br> Sedangkan umur ibu adalah $usiaibu tahun.<br>";
+
+?>
