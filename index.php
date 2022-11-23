4bdbb7b63e5323336e47a08e18489e379e46b116
Hana Kurniasari <Kurniasarihana@gmail.com>
2022-11-23 15:39:19 +0700

    buat PHP CRUD Handling

--- index.php
+++ index.php
@@ -1,24 +1,68 @@
 <?php
 
-echo "Hello World!<br>";
+// 1. Buat koneksi dengan MySQL
+$con = mysqli_connect("localhost","root","","fakultas");
 
-$nama = "Hana Kurniasari";
-$usia = 22;
+// 2. Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}else{
+    echo 'koneksi berhasil';
+}
 
-echo "Nama saya $nama, saya berusia $usia tahun.<br>";
+// 3 buat query baca semua data dari table
+$sql = "SELECT * FROM mahasiswa";
 
-$namakakak = "Kurnia Ramadani";
-$usiakakak = 25;
+// 4. tampilkan data, cek apakah query bisa dijalankan
+$mahasiswa = [];
+if ($result = mysqli_query($con, $sql)) {
+    // tampilkan satu per satu
+    while ($row = mysqli_fetch_assoc($result)) {
+        $mahasiswa[] = $row;
+    }
+    mysqli_free_result($result);
+  }
 
-echo "Saya memiliki satu orang kakak perempuan yang bernama $namakakak, kakak saya berusia $usiakakak tahun.<br>";
-
-
-$selisihumur = $usiakakak - $usia;
-echo "Selisih usia saya dan kakak saya adalah $selisihumur tahun.<br>";
-
-
-$usiaibu = 50;
-$usiajumlah = $usiakakak + $usia;
-echo "Jika dijumlahkan maka jumlah total umur kami adalah $usiajumlah tahun.<br> Sedangkan umur ibu adalah $usiaibu tahun.<br>";
-
+// 5. tutup koneksi
+mysqli_close($con);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    <a href="insert.php">Tambah Data</a>
+    <table border=1 style="width: 100%;">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Jenis Kelamin</th>
+            <th>Tempat Lahir</th>
+            <th>Tanggal Lahir</th>
+            <th>Alamat</th>
+            <th>Action</th>
+        </tr>
+        <?php foreach ($mahasiswa as $row): ?>
+            <tr>
+                <td><?= $row['nim'] ?></td>
+                <td><?= $row['nama'] ?></td>
+                <td><?= $row['jenis_kelamin'] ?></td>
+                <td><?= $row['tempat_lahir'] ?></td>
+                <td><?= $row['tanggal_lahir'] ?></td>
+                <td><?= $row['alamat'] ?></td>
+                <td>
+                    <a href="update.php?id=<?= $row['id'] ?>" >Edit</a> | 
+                    <a href="delete.php?id=<?= $row['id'] ?>" >Delete</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
